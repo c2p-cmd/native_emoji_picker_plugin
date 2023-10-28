81cfdc18214a0c60b721198d1cf6f05e7e36246f
Sharan Thakur <sharandt19@live.com>
2023-10-28 18:03:11 +0530

    added swift implementation

---
+++ ios/Classes/EmojiPickerController.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiPickerController.swift
+//  native_emoji_picker
+//
+//  Created by Sharan Thakur on 28/10/23.
+//
+
+import Foundation
---
+++ ios/Classes/UIApplication+firstKeyWindow.swift
@@ -0,0 +1,8 @@
+//
+//  UIApplication+firstKeyWindow.swift
+//  native_emoji_picker
+//
+//  Created by Sharan Thakur on 28/10/23.
+//
+
+import Foundation
